dfb8670eb7ebda5f725277ee8526cf40c49260dd
dariusz-hq <dariusz.huk@gmail.com>
2025-12-12 23:44:11 +0000

    Functions exercise

--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -2,22 +2,48 @@
 //  Use functions to perform basic operations and print some
 //  information to the terminal.
 //
-//--Requirements:
-//* Write a function that accepts a person's name as a function
-//  parameter and displays a greeting to that person.
-//* Write a function that returns any message, and call it from within
-//  fmt.Println()
-//* Write a function to add 3 numbers together, supplied as arguments, and
-//  return the answer
-//* Write a function that returns any number
-//* Write a function that returns any two numbers
-//* Add three numbers together using any combination of the existing functions.
-//  * Print the result
-//* Call every function at least once
 
 package main
 
-func main() {
+import "fmt"
 
+//   - Write a function that accepts a person's name as a function
+//     parameter and displays a greeting to that person.
+func greet(name string) {
+	fmt.Println("hello", name)
 }
 
+//   - Write a function that returns any message, and call it from within
+//     fmt.Println()
+func message() string {
+	return "some message to be returned"
+}
+
+//   - Write a function to add 3 numbers together, supplied as arguments, and
+//     return the answer
+func add(n1, n2, n3 int) int {
+	return n1 + n2 + n3
+}
+
+// * Write a function that returns any number
+func returnNumber() int {
+	return 9
+}
+
+// * Write a function that returns any two numbers
+func returnTwoNumbers() (int, int) {
+	return 1, 2
+}
+
+func main() {
+	//--Requirements:
+	//* Call every function at least once
+	greet("John Doe")
+	fmt.Println(message())
+	fmt.Println(returnNumber())
+	fmt.Println(returnTwoNumbers())
+
+	//* Add three numbers together using any combination of the existing functions.
+	//  * Print the result
+	fmt.Println(add(1, 2, 3))
+}
