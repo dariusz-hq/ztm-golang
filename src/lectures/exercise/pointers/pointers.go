d6e9b5f881f9b67d114391dc27cfad99bd8bbd89
dariusz-hq <dariusz.huk@gmail.com>
2025-12-20 00:22:52 +0000

    Pointer exercise

--- src/lectures/exercise/pointers/pointers.go
+++ src/lectures/exercise/pointers/pointers.go
@@ -3,8 +3,7 @@
 //  on products.
 //
 //--Requirements:
-//* Create a structure to store items and their security tag state
-//  - Security tags have two states: active (true) and inactive (false)
+
 //* Create functions to activate and deactivate security tags using pointers
 //* Create a checkout() function which can deactivate all tags in a slice
 //* Perform the following:
@@ -16,7 +15,42 @@
 
 package main
 
-func main() {
+import "fmt"
 
+type Product struct {
+	name string
+	tag  bool
 }
 
+func activate(item *Product) {
+	item.tag = true
+}
+
+func deactivate(item *Product) {
+	item.tag = false
+}
+
+func checkout(products []Product) {
+	for i := range products {
+		deactivate(&products[i])
+	}
+}
+
+func main() {
+	//* Create a structure to store items and their security tag state
+	//  - Security tags have two states: active (true) and inactive (false)
+	products := []Product{
+		{"item1", true},
+		{"item2", true},
+		{"item3", true},
+		{"item4", true},
+	}
+	fmt.Println("products", products)
+
+	deactivate(&products[0])
+	fmt.Println("after deactivating", products)
+
+	checkout(products)
+	fmt.Println("after checkout", products)
+
+}
