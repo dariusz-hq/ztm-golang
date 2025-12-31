ab638791e7da35f329a368e249c9d6c5fc09ea9a
dariusz-hq <dariusz.huk@gmail.com>
2025-12-31 12:06:59 +0000

    Testing exercise

--- src/lectures/exercise/testing/rcv-func_test.go
+++ src/lectures/exercise/testing/rcv-func_test.go
@@ -1,3 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
 //--Summary:
 //  Copy your rcv-func solution to this directory and write unit tests.
 //
@@ -12,3 +19,62 @@
 //* Use `go test -v ./exercise/testing` to run these specific tests
 //
 
+func TestHealthEnergyCannotGoAboveMaximum(t *testing.T) {
+	testCases := []struct {
+		health    Health
+		maxHealth Health
+		energy    Energy
+		maxEnergy Energy
+		name      string
+	}{
+		{1, 1, 1, 1, "name1"},
+		{1, 10, 1, 10, "name1"},
+	}
+
+	for _, data := range testCases {
+		p := Player{data.health, data.maxHealth, data.energy, data.maxEnergy, data.name}
+		for i := Health(0); i <= data.maxHealth+1; i++ {
+			p.incrementHealth()
+			if p.health > p.maxHealth {
+				t.Errorf("Health is %v, want %v", p.health, p.maxHealth)
+			}
+			fmt.Println(p)
+		}
+		for i := Energy(0); i <= data.maxEnergy+1; i++ {
+			p.incrementEnergy()
+			if p.energy > p.maxEnergy {
+				t.Errorf("Energy is %v, want %v", p.energy, p.maxEnergy)
+			}
+			fmt.Println(p)
+		}
+	}
+}
+
+func TestHealthEnergyCannotGoBelowZero(t *testing.T) {
+	testCases := []struct {
+		health    Health
+		maxHealth Health
+		energy    Energy
+		maxEnergy Energy
+		name      string
+	}{
+		{1, 1, 1, 1, "name1"},
+		{1, 10, 1, 10, "name1"},
+	}
+
+	for _, data := range testCases {
+		p := Player{data.health, data.maxHealth, data.energy, data.maxEnergy, data.name}
+		for i := p.maxHealth; i < 0; i-- {
+			p.decrementHealth()
+			if p.health < 0 {
+				t.Errorf("Health is %v, want %v", p.health, 0)
+			}
+		}
+		for i := p.maxEnergy; i < 0; i-- {
+			p.decrementEnergy()
+			if p.energy < 0 {
+				t.Errorf("Energy is %v, want %v", p.energy, 0)
+			}
+		}
+	}
+}
