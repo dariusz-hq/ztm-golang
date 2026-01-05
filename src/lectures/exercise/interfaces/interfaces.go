2ce6f1b3c84d0c7bd7a782649e75748b5223e4d6
dariusz-hq <dariusz.huk@gmail.com>
2026-01-05 21:18:09 +0000

    Interface exercise

--- src/lectures/exercise/interfaces/interfaces.go
+++ src/lectures/exercise/interfaces/interfaces.go
@@ -19,7 +19,36 @@
 
 package main
 
-func main() {
+import "fmt"
 
+type Lifter interface {
+	Lift()
 }
 
+type Motorcycle string
+type Car string
+type Truck string
+
+func (m Motorcycle) Lift() {
+	fmt.Println("Using small lift")
+}
+
+func (c Car) Lift() {
+	fmt.Println("Using standard lift")
+}
+
+func (t Truck) Lift() {
+	fmt.Println("Using large lift")
+}
+
+func LiftVehicle(l Lifter) {
+	fmt.Printf("Lifting vehicle %v \n", l)
+}
+
+func main() {
+	vehicles := []Lifter{Motorcycle("Ducati"), Car("VW Passat"), Truck("Ford Ranger")}
+	for _, v := range vehicles {
+		fmt.Printf("Handling lifting for %v \n", v)
+		v.Lift()
+	}
+}
